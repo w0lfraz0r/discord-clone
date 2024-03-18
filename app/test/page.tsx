673960fe082bda4e3d2ae1ea65be43f56d44b1d5
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2024-03-18 17:23:55 +0530

    routes practice

---
+++ app/test/page.tsx
@@ -0,0 +1,11 @@
+const TestPage = () => {
+    return ( 
+        <div>
+            <p className="text-3xl font bold text-indigo-500">
+                Discord Test page
+            </p>
+        </div>
+     );
+}
+ 
+export default TestPage;
