943a80e9c4c39d9fc65f27ea8da136bd40a6b5d8
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2024-03-18 18:06:53 +0530

    added sign in and signout

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font bold text-indigo-500">
-        This is a protected route
-      </p>
+      <UserButton
+        afterSignOutUrl="/"
+      />
     </div>
   );
 }
