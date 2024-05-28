35e8d2c9d5d48ec696a4479fcd9d55900f8898e5
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2024-05-28 15:43:18 +0530

    added preview for server image

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -14,18 +14,10 @@ const handleAuth = () => {
 export const ourFileRouter = {
     serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
         .middleware(() => handleAuth())
-        .onUploadComplete(async ({ metadata, file }) => {
-            console.log("Upload complete for userId:", metadata.userId);
-            console.log("file url", file.url);
-            return { uploadedBy: metadata.userId };
-        }),
+        .onUploadComplete(() => { }),
     messageFile: f(["image", "pdf"])
         .middleware(() => handleAuth())
-        .onUploadComplete(async ({ metadata, file }) => {
-            console.log("Upload complete for userId:", metadata.userId);
-            console.log("file url", file.url);
-            return { uploadedBy: metadata.userId };
-        }),
+        .onUploadComplete(() => { }),
 
 } satisfies FileRouter;
 
