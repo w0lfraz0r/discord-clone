ee5e0af4ce0b1fad320a14d3858631f7016d2634
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2024-05-28 15:22:40 +0530

    added uploadthing

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,32 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+import { auth } from "@clerk/nextjs";
+
+const f = createUploadthing();
+
+const handleAuth = () => {
+    const { userId } = auth();
+    if (!userId) throw new UploadThingError("Unauthorized");
+    return { userId: userId };
+}
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+    serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+        .middleware(() => handleAuth())
+        .onUploadComplete(async ({ metadata, file }) => {
+            console.log("Upload complete for userId:", metadata.userId);
+            console.log("file url", file.url);
+            return { uploadedBy: metadata.userId };
+        }),
+    messageFile: f(["image", "pdf"])
+        .middleware(() => handleAuth())
+        .onUploadComplete(async ({ metadata, file }) => {
+            console.log("Upload complete for userId:", metadata.userId);
+            console.log("file url", file.url);
+            return { uploadedBy: metadata.userId };
+        }),
+
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -25,6 +25,7 @@ import {
 import { Button } from "../../components/ui/button";
 import { Input } from "../../components/ui/input";
 import React, { useEffect, useState } from "react";
+import FileUpload from "../file-upload";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: 'Server name is required' }),
@@ -71,7 +72,21 @@ const InitialModal = () => {
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
-                Todo: Image upload
+                <FormField
+                  control={form.control}
+                  name="imageUrl"
+                  render={({ field }) => {
+                    return <FormItem>
+                      <FormControl>
+                        <FileUpload
+                          endpoint="serverImage"
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  }}
+                />
               </div>
               <FormField
                 control={form.control}
