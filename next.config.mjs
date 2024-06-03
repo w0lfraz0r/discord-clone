63caf7f77b787d63a8e48760629b7c2813615725
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2024-06-03 21:10:21 +0530

    removed dangerous sitew fixes

--- next.config.mjs
+++ next.config.mjs
@@ -14,39 +14,6 @@ const nextConfig = {
 
     swcMinify: true,
 
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Content-Security-Policy',
-                        value: "default-src 'self'; img-src 'self' data:; script-src 'self'; style-src 'self' 'unsafe-inline';",
-                    },
-                    {
-                        key: 'Strict-Transport-Security',
-                        value: 'max-age=63072000; includeSubDomains; preload',
-                    },
-                    {
-                        key: 'X-Content-Type-Options',
-                        value: 'nosniff',
-                    },
-                    {
-                        key: 'X-Frame-Options',
-                        value: 'DENY',
-                    },
-                    {
-                        key: 'X-XSS-Protection',
-                        value: '1; mode=block',
-                    },
-                    {
-                        key: 'Referrer-Policy',
-                        value: 'no-referrer-when-downgrade',
-                    },
-                ],
-            },
-        ];
-    },
 };
 
 export default nextConfig;
