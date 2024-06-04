a675711ccb20057f602081e1ad8807fdd0a09c4e
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2024-06-04 13:42:05 +0530

    added delete server

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -36,3 +36,33 @@ export async function PATCH(
         return new NextResponse("Internal Server Error", { status: 500 });
     }
 }
+
+export async function DELETE(
+    req: Request,
+    { params }: { params: { serverId: string } }
+) {
+    try {
+        const profile = await currentProfile();
+
+        if (!profile) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        if (!params.serverId) {
+            return new NextResponse("Server ID missing", { status: 400 });
+        }
+
+        const server = await db.server.delete({
+            where: {
+                id: params.serverId,
+                profileId: profile.id,
+            },
+        });
+
+        return NextResponse.json(server);
+
+    } catch (error) {
+        console.log("[SERVER_ID_DELETE] Error: " + error);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+}
