7bf1966053fe27c458bc7b3adba8603f495077ac
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2024-06-04 15:30:00 +0530

    removed helmet

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,13 +1,8 @@
 import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
-import { Helmet } from 'react-helmet';
-
 
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
     return (
         <div className="h-full">
-            <Helmet>
-                <meta name="google-site-verification" content="hTwJDS3uUs3_9oea-NXkWtcS_3wXX_sGuC7iVL5jdg4" />
-            </Helmet>
             <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
                 <NavigationSidebar />
             </div>
