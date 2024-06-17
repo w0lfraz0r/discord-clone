725e3be457ae761daa8ac7ed71b845e7a7e89c4b
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2024-06-17 11:36:24 +0530

    added logs for connect and disconnect

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -13,6 +13,7 @@ export const config = {
 
 const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
     if (!res.socket.server.io) {
+        console.log("Initializing Socket.io server...");
         const path = "/api/socket/io";
         const httpServer: NetServer = res.socket.server as any;
         const io = new ServerIO(httpServer, {
@@ -20,5 +21,18 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
             addTrailingSlash: false,
         });
         res.socket.server.io = io;
+
+        io.on("connection", (socket) => {
+            console.log("New client connected");
+
+            socket.on("disconnect", () => {
+                console.log("Client disconnected");
+            });
+        });
+    } else {
+        console.log("Socket.io server already running.");
     }
+    res.end();
 }
+
+export default ioHandler;
