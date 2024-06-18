382c206a824a8ada201242de10a8c855cebe7d1c
vaibhav0shrivas <vaibhavshrivasone@gmail.com>
2024-06-18 12:25:08 +0530

    added chat socket

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import ChatItem from "./chat-item";
 import { format } from "date-fns";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 const DATE_FORMAT = "d MMM yyyy HH:mm";
 
@@ -40,6 +41,8 @@ const ChatMessages = ({
     type,
 }: ChatMessagesProps) => {
     const queryKey = `chat:${chatId}`;
+    const addKey = `chat:${chatId}:messages`;
+    const updateKey = `chat:${chatId}:messages:update`;
     const {
         data,
         fetchNextPage,
@@ -51,7 +54,9 @@ const ChatMessages = ({
         apiUrl,
         paramKey,
         paramValue,
-    })
+    });
+
+    useChatSocket({queryKey, addKey, updateKey});
 
     if (status === "pending") {
         return (
